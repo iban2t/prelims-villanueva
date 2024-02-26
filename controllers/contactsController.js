65d7d9461fa160b1921ef4f6e25bf8b785018a65
iban2t <joshuaivanvillanueva@gmail.com>
2024-02-27 00:24:21 +0800

    update contacts

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -7,7 +7,7 @@ exports.addContact = async (req, res) => {
         const { name, type, num, username } = req.body;
 
         const addContactQuery = 'INSERT INTO contacts (name, type, num, user_id, username) VALUES (?, ?, ?, ?, ?)';
-        await db.execute(addContactQuery, [name, type, num, userId, username]);
+        await db.promise().execute(addContactQuery, [name, type, num, userId, username]);
 
         res.status(201).json({ message: 'Contact added successfully' });
     } catch (error) {
@@ -22,7 +22,7 @@ exports.getContacts = async (req, res) => {
         const userId = req.userId;
 
         const getAllContactsQuery = 'SELECT * FROM contacts WHERE user_id = ?';
-        const [contacts] = await db.execute(getAllContactsQuery, [userId]);
+        const [contacts] = await db.promise().execute(getAllContactsQuery, [userId]);
 
         res.json(contacts);
     } catch (error) {
@@ -38,7 +38,7 @@ exports.getContact = async (req, res) => {
         const contactId = req.params.id;
 
         const getContactQuery = 'SELECT * FROM contacts WHERE id = ? AND user_id = ?';
-        const [contact] = await db.execute(getContactQuery, [contactId, userId]);
+        const [contact] = await db.promise().execute(getContactQuery, [contactId, userId]);
 
         if (contact.length === 0) {
             return res.status(404).json({ error: 'Contact not found' });
@@ -59,7 +59,7 @@ exports.updateContact = async (req, res) => {
         const { name, type, num, username } = req.body;
 
         const updateContactQuery = 'UPDATE contacts SET name = ?, type = ?, num = ?, username = ? WHERE id = ? AND user_id = ?';
-        const [result] = await db.execute(updateContactQuery, [name, type, num, username, contactId, userId]);
+        const [result] = await db.promise().execute(updateContactQuery, [name, type, num, username, contactId, userId]);
 
         if (result.affectedRows === 0) {
             return res.status(404).json({ error: 'Contact not found or unauthorized' });
@@ -79,7 +79,7 @@ exports.deleteContact = async (req, res) => {
         const contactId = req.params.id;
 
         const deleteContactQuery = 'DELETE FROM contacts WHERE id = ? AND user_id = ?';
-        const [result] = await db.execute(deleteContactQuery, [contactId, userId]);
+        const [result] = await db.promise().execute(deleteContactQuery, [contactId, userId]);
 
         if (result.affectedRows === 0) {
             return res.status(404).json({ error: 'Contact not found or unauthorized' });
